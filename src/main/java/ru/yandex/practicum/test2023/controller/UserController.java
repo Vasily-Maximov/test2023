e404c5173430f091b29c12e3c8b5bc67c859020b
Vasily Maximov <softservice-31@mail.ru>
2023-05-31 17:46:49 +0300

    add in Controller delUsers

--- src/main/java/ru/yandex/practicum/test2023/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/test2023/controller/UserController.java
@@ -1,10 +1,7 @@
 package ru.yandex.practicum.test2023.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.test2023.model.User;
 
 import java.util.ArrayList;
@@ -30,4 +27,10 @@ public class UserController {
     public  List<User> getAllUsers() {
         return users;
     }
+
+    @DeleteMapping()
+    public void delUsers() {
+        users.clear();
+    }
+
 }
