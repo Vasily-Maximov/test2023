5ec888b5aa5321ba38ebf12ffb287fa324f9467a
Vasily Maximov <softservice-31@mail.ru>
2023-05-31 17:06:48 +0300

    initial commit

---
+++ src/main/java/ru/yandex/practicum/test2023/controller/UserController.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.test2023.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+
+
+}
---
+++ src/test/java/ru/yandex/practicum/test2023/Test2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.test2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
