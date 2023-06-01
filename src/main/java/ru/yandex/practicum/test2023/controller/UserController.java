c29d4b54f5b4bb179c5198cfd5752dd6b9fbc6bb
Vasily Maximov <softservice-31@mail.ru>
2023-06-01 10:39:54 +0300

    add import package

--- src/main/java/ru/yandex/practicum/test2023/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/test2023/controller/UserController.java
@@ -1,7 +1,12 @@
 package ru.yandex.practicum.test2023.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import ru.yandex.practicum.test2023.model.User;
 
 import java.util.ArrayList;
